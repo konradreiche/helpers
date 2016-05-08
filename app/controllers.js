b9f73b3f6e31798c11723dba37f5f99c395e720e
Konrad Reiche <konrad@mixlr.com>
2016-05-09 00:04:24 +0200

    Make question submission responsive

--- app/controllers.js
+++ app/controllers.js
@@ -4,14 +4,18 @@ const helpersControllers = angular.module('helpersControllers', []);
 
 helpersControllers.controller('QuestionCtrl', ['$scope', 'Question', function ($scope, Question) {
   $scope.orderProp = 'age';
+  $scope.questions = Question.query();
 
   $scope.submitQuestion = function() {
     if ($scope.questionText) {
       $scope.question = new Question({text: $scope.questionText});
       $scope.question.text = $scope.questionText;
-      $scope.question.$save();
+      $scope.question.$save(function() {
+        $scope.questionText = null;
+        $scope.questions = Question.query();
+      });
     }
   };
 
-  $scope.questions = Question.query();
+
 }]);
--- modules/questions.js
+++ modules/questions.js
@@ -6,11 +6,12 @@ module.exports = {
   create: function(req, res) {
     redis.incrAsync('question:id')
     .then(redis.getAsync('question:id'))
-    .then((id) => redis.setAsync(`question:${id}`, req.body.text));
+    .then((id) => redis.setAsync(`question:${id}`, req.body.text))
+    .then(() => res.sendStatus(200))
   },
 
   query: function(req, result) {
-    redis.scanAsync('0', 'MATCH', 'question:*', 'COUNT', '100')
+    redis.scanAsync('0', 'MATCH', 'question:[^id]', 'COUNT', '100')
     .spread((cursor, keys) => Promise.resolve(keys))
     .map((ids) => redis.getAsync(ids))
     .then((questions) => result.send(questions.map((text) => { return {text: text} })))
