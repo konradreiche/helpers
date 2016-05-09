abc69a0361bc485f1a8a80b1ec57091ec3a8c2a7
Konrad Reiche <konrad@mixlr.com>
2016-05-10 00:04:31 +0200

    Use question id to generate the HTML link

--- modules/questions.js
+++ modules/questions.js
@@ -1,19 +1,27 @@
 "use strict";
 
-const redis = require('./redis');
+const redis   = require('./redis');
+const Promise = require('bluebird');
+
+function getQuestion(id) {
+  return redis.getAsync(id).then(function(question) {
+    id = id.split(':').pop();
+    return Promise.resolve({id: id, text: question});
+  });
+}
 
 module.exports = {
   create: function(req, res) {
     redis.incrAsync('question:id')
     .then(redis.getAsync('question:id'))
     .then((id) => redis.setAsync(`question:${id}`, req.body.text))
-    .then(() => res.sendStatus(200))
+    .then(() => res.sendStatus(200));
   },
 
   query: function(req, result) {
     redis.scanAsync('0', 'MATCH', 'question:[^id]', 'COUNT', '100')
     .spread((cursor, keys) => Promise.resolve(keys))
-    .map((ids) => redis.getAsync(ids))
-    .then((questions) => result.send(questions.map((text) => { return {text: text} })))
+    .map((id) => getQuestion(id))
+    .then((obj) => result.send(obj));
   }
 };
